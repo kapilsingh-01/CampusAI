8421f098cf5f75ddd05dd1fe8e20894ac881f2bb
Kapil Singh Rathore <kapilathore5@gmail.com>
2026-08-09 22:50:27 +0530

    Fix PDF upload API URL

--- src/components/PDFUploader.jsx
+++ src/components/PDFUploader.jsx
@@ -25,7 +25,7 @@ function PDFUploader() {
             formData.append("pdf", file);
 
             const response = await axios.post(
-                "http://localhost:5000/api/upload",
+    `${import.meta.env.VITE_API_URL}/api/upload`,
                 formData,
                 {
                     headers: {
